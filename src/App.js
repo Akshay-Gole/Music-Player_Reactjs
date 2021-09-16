3c132f835be507c113c434594952896bdf925f4a
Akshay <akshay.gole@nineleaps.com>
2021-09-16 12:31:08 +0530

    Trying to add Google Analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 // adding components
 import Song from './component/song'
 import Player from './component/player'
@@ -9,55 +9,65 @@ import './styles/app.scss'
 //  importing music's from util
 import Music from './data'
 
+// Google Analytics
+import ReactGa from 'react-ga'
+
 function App() {
+
+  useEffect(() => {
+    ReactGa.initialize('G-Z9XWJ279BC')
+
+    ReactGa.pageview(window.location.pathname + window.location.search)
+  }, [])
+
   // here Music() will return the list of objects which have all data 
-  const [songs,setSongs]= useState(Music())
-  
+  const [songs, setSongs] = useState(Music())
+
   // here song[0] shows the song for the very first time
-  const [currentSong,setCurrentSong]=useState(songs[0])
+  const [currentSong, setCurrentSong] = useState(songs[0])
 
   // for pasue and replay the song
-  const [playing,setPlaying] = useState(false)
+  const [playing, setPlaying] = useState(false)
 
   // state for showing the time of the song
-  const [currentTime,setCurrentTime] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0)
 
   // state for showing the time of the song 
-  const [duration, setDuration]= useState(0)
+  const [duration, setDuration] = useState(0)
 
   // State for library button, Is it open or not open
-  const [libraryStatus,setLibraryStatus]= useState(false)
+  const [libraryStatus, setLibraryStatus] = useState(false)
 
   // using ref for getting the audio HTML element
-  const audioRef= useRef()
+  const audioRef = useRef()
 
   // Time and duration event handler ( here currentTime andf Duration are not states they are values of audio element)
-  const timeHandler = (e)=>{
+  const timeHandler = (e) => {
     setCurrentTime(e.target.currentTime)
     setDuration(e.target.duration)
-}
-  const endHandler =async ()=>{
-    const currentIndex = songs.findIndex((song)=> song.id===currentSong.id)
-    await setCurrentSong(songs[(currentIndex +1)% songs.length])
-    if(playing){
+  }
+  const endHandler = async () => {
+    const currentIndex = songs.findIndex((song) => song.id === currentSong.id)
+    await setCurrentSong(songs[(currentIndex + 1) % songs.length])
+    if (playing) {
       audioRef.current.play()
     }
   }
-  
+
   return (
     <div className={`App ${libraryStatus ? 'move-right' : ''}`}>
-      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus}/>
-      <Song currentSong={currentSong}/>
+      <Nav libraryStatus={libraryStatus} setLibraryStatus={setLibraryStatus} />
+      <Song currentSong={currentSong} />
 
-      <Player currentTime={currentTime} setCurrentTime={setCurrentTime} duration={duration}  
-        playing={playing} setPlaying={setPlaying} 
+      <Player currentTime={currentTime} setCurrentTime={setCurrentTime} duration={duration}
+        playing={playing} setPlaying={setPlaying}
         currentSong={currentSong} audioRef={audioRef}
-        songs={songs} setSongs={setSongs} setCurrentSong={setCurrentSong} currentSong={currentSong}/>
+        songs={songs} setSongs={setSongs} setCurrentSong={setCurrentSong} currentSong={currentSong} />
 
-      <Library libraryStatus={libraryStatus} playing={playing} setCurrentSong={setCurrentSong} audioRef={audioRef} songs={songs} setSongs={setSongs}/>
+      <Library libraryStatus={libraryStatus} playing={playing} setCurrentSong={setCurrentSong} audioRef={audioRef} songs={songs} setSongs={setSongs} />
 
       {/* Audio player */}
-      <audio onTimeUpdate={timeHandler} onLoadedMetadata={timeHandler} ref ={audioRef} src={currentSong.audio} onEnded={endHandler} ></audio>
+      <audio onTimeUpdate={timeHandler} onLoadedMetadata={timeHandler} ref={audioRef} src={currentSong.audio} onEnded={endHandler} ></audio>
     </div>
   );
 }
--- src/component/Nav.js
+++ src/component/Nav.js
@@ -1,13 +1,25 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMusic } from '@fortawesome/free-solid-svg-icons'
+import ReactGa from 'react-ga'
 
-function Nav({setLibraryStatus, libraryStatus}) {
+function Nav({ setLibraryStatus, libraryStatus }) {
+
+    const ClickHandler = () => {
+        ReactGa.event({
+            category: "Button",
+            action: "Clicked the Lib button"
+        })
+    }
+
     return (
         <nav className='nav'>
             <h1>Mp3 Player</h1>
-            <button onClick={()=>setLibraryStatus(!libraryStatus)}>
-                Library 
+            <button onClick={() => {
+                setLibraryStatus(!libraryStatus)
+                ClickHandler()
+            }}>
+                Library
                 <FontAwesomeIcon icon={faMusic} />
             </button>
         </nav>
